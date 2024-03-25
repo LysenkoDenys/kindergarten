64eb514d3c2d480b52b89a0295fd116f5a934117
Lysenko <lysenkoden@gmail.com>
2024-03-25 23:18:27 +0100

    add section on main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
 const Home = () => {
-  return <div>Home</div>;
+  const date = new Date();
+  const currentDate = date.toISOString().split('T')[0];
+  return (
+    <section className="select-none">
+      <h1 className="text-3xl font-bold text-center mb-3 mt-3">
+        Опис групи 1979-1980 р.н.:
+      </h1>
+      <div className="bg-[#e6e7eb] rounded-[5px] p-1 mb-4 text-[1em] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] hover:bg-[#e7e8e9] hover:shadow-[2px_2px_2px_rgba(0,0,0,0.3)]">
+        <article>
+          <h3 className="text-xl font-bold">Загальна інформація.</h3>
+          <p>
+            Станом на {currentDate} садочек розформовано і він існує тільки в
+            спогадах.
+          </p>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
+
+//shadows
