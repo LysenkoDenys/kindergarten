424129b30397dbbb8341c6cdef11b3857bd20bc3
Lysenko <lysenkoden@gmail.com>
2024-03-26 23:39:12 +0100

    add photo and button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,8 +23,8 @@ const Home = () => {
             розформовано та існує тільки в наших спогадах.
           </p>
           <p>
-            &ensp; Протягом терміну виховання крізь групу пройшло {}
-            {groupMates.length} особистості:
+            &ensp; Протягом терміну виховання крізь групу пройшло щонайменьше {}
+            {groupMates.length} визначені особистості (цифра не остаточна):
           </p>
           <ul className="ml-6">
             <li>
@@ -37,9 +37,18 @@ const Home = () => {
               дівчаток: {femalesQty} ({femalesQtyDeg}%);
             </li>
           </ul>
+          <p>
+            &ensp; Частину одногрупників (27 осіб) представлено на фото нижче:
+          </p>
+          <div className="flex justify-center items-center flex-col">
+            <img src={require('../assets/main-01.jpg')} alt="my group mates" />
+            <button className="bg-gray-400 hover:bg-gray-500 hover:text-white  font-bold py-2 px-4 m-3 rounded-[5px] shadow hover:shadow-lg">
+              Перелік одногрупників на фото
+            </button>
+          </div>
         </article>
       </div>
-      <div className="bg-[#e6e7eb] rounded-[5px] p-1 mb-4 text-[1em] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] hover:bg-[#e7e8e9] hover:shadow-[2px_2px_2px_rgba(0,0,0,0.3)]">
+      <div className="bg-[#e6e7eb] rounded-[5px] p-1 mb-4 text-[1em] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] hover:bg-[#e7e8e9] ">
         <article>
           <h3 className="text-xl font-bold">Додаткова інформація.</h3>
           <p>&ensp; Основні виховательки:</p>
