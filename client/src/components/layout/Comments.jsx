4b35988d56ca8ecc06ee1be0d2b86c5c6c9892b0
Lysenko <lysenkoden@gmail.com>
2024-04-13 22:19:45 +0200

    fix textarea and add some stories

--- client/src/components/layout/Comments.jsx
+++ client/src/components/layout/Comments.jsx
@@ -7,8 +7,8 @@ const Comments = () => {
   const [text, setText] = useState('');
   const darkTheme = useTheme();
   const themeComment = darkTheme
-    ? 'px-3 rounded-[10px] w-[70%] mr-1 my-3 shadow-[2px_2px_2px_2px_rgba(255,255,255,0.8)] text-black outline-none'
-    : 'px-3 rounded-[10px] w-[70%] mr-1 my-3 shadow-[5px_5px_5px_5px_rgba(0,0,0,0.4)] outline-none';
+    ? 'px-3 py-1.5 rounded-[10px] w-[70%] mr-1 my-3 shadow-[2px_2px_2px_2px_rgba(255,255,255,0.8)] text-black outline-none leading-[18px]'
+    : 'px-3 py-1.5 rounded-[10px] w-[70%] mr-1 my-3 shadow-[5px_5px_5px_5px_rgba(0,0,0,0.4)] outline-none';
 
   const handleChange = (event) => setText(event.target.value);
 
