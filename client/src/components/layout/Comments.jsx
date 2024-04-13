919f93bbce6e6582ea7de1e35e0a69fd9cf0ecc5
Lysenko <lysenkoden@gmail.com>
2024-04-13 07:25:44 +0200

    fix Comments

--- client/src/components/layout/Comments.jsx
+++ client/src/components/layout/Comments.jsx
@@ -7,15 +7,15 @@ const Comments = () => {
   const [text, setText] = useState('');
   const darkTheme = useTheme();
   const themeComment = darkTheme
-    ? 'px-3 rounded-[10px] w-[70%] mr-1 my-3 shadow-[2px_2px_2px_2px_rgba(255,255,255,0.8)] text-black'
-    : 'px-3 rounded-[10px] w-[70%] mr-1 my-3 shadow-[5px_5px_5px_5px_rgba(0,0,0,0.4)]';
+    ? 'px-3 rounded-[10px] w-[70%] mr-1 my-3 shadow-[2px_2px_2px_2px_rgba(255,255,255,0.8)] text-black outline-none'
+    : 'px-3 rounded-[10px] w-[70%] mr-1 my-3 shadow-[5px_5px_5px_5px_rgba(0,0,0,0.4)] outline-none';
 
   const handleChange = (event) => setText(event.target.value);
 
   return (
     <Block>
+      <h3 className="text-xl font-bold text-center">Коментарі:</h3>
       <div className="flex justify-center items-center" id="comments">
-        <h2></h2>
         <textarea
           className={themeComment}
           placeholder="Текст коментаря..."
