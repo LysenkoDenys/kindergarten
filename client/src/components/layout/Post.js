6fcc17516997e4942a70b92a7426dc575063570e
Lysenko <lysenkoden@gmail.com>
2024-04-21 23:11:49 +0200

    add stories, add comments, fake cookies

--- client/src/components/layout/Post.js
+++ client/src/components/layout/Post.js
@@ -1,16 +1,34 @@
 import { usePost } from '../../context/PostContext';
+import { useAsyncFn } from '../../hooks/useAsync';
+import { createComment } from '../../services/comments';
 import CommentForm from './CommentForm';
 import CommentList from './CommentList';
 
 const Post = () => {
   const { post, rootComments } = usePost();
+  const {
+    loading,
+    error,
+    execute: createCommentFn,
+  } = useAsyncFn(createComment);
+
+  function onCommentCreate(message) {
+    return createCommentFn({ postId: post.id, message }).then((comment) => {
+      console.log(comment);
+    });
+  }
+
   return (
     <>
       <h1>{post.title}</h1>
       <article>{post.body}</article>
       <h3 className="text-[24px]">Comments</h3>
       <section>
-        <CommentForm loading error onSubmit />
+        <CommentForm
+          loading={loading}
+          error={error}
+          onSubmit={onCommentCreate}
+        />
         {rootComments != null && rootComments.length > 0 && (
           <div className="mt-4">
             <CommentList comments={rootComments} />
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import fastify from 'fastify';
 import sensible from '@fastify/sensible';
+import cookie from '@fastify/cookie';
 import cors from '@fastify/cors';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
@@ -8,11 +9,28 @@ dotenv.config();
 
 const app = fastify();
 app.register(sensible);
+app.register(cookie, { secret: process.env.COOKIE_SECRET });
 app.register(cors, {
   origin: process.env.CLIENT_URL,
   credentials: true,
 });
+
+//fake cookie (if we make a request --> set cookie on our browser to the user ID):
+app.addHook('onRequest', (req, res, done) => {
+  if (req.cookies.userId !== CURRENT_USER_ID) {
+    req.cookies.userId = CURRENT_USER_ID;
+    res.clearCookie('userId');
+    res.setCookie('userId', CURRENT_USER_ID);
+  }
+  done();
+});
+//==============================================
 const prisma = new PrismaClient();
+//fake cookie:
+const CURRENT_USER_ID = await prisma.user.findFirst({
+  where: { name: 'Kyle' },
+}).id;
+//==============================================
 
 app.get('/posts', async (req, res) => {
   return await commitToDb(
@@ -51,6 +69,22 @@ app.get('/posts/:id', async (req, res) => {
             },
           },
         },
+      },
+    })
+  );
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  if (req.body.message === '' || req.body.message == null) {
+    return res.send(app.httpErrors.badRequest('Message is required'));
+  }
+  return await commitToDb(
+    prisma.comment.create({
+      data: {
+        message: req.body.message,
+        userId: req.cookies.userId,
+        parentId: req.body.parentId,
+        postId: req.params.id,
       },
     })
   );
