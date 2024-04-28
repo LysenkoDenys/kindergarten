d7bbb31ee27fec327c4d5cdd14629636dc5e888b
Lysenko <lysenkoden@gmail.com>
2024-04-28 23:15:32 +0200

    fix getUrl and Memo, add a story

--- client/src/data/getUrl.js
+++ client/src/data/getUrl.js
@@ -1,16 +1,18 @@
 const getUrl = () => {
   const hash = window.location.hash;
-  if (hash) {
-    const id = hash.substring(1); // Remove the '#' character
-    const element = document.getElementById(id);
-    if (element) {
-      const topOffset = element.getBoundingClientRect().top;
-      window.scrollTo({
-        top: window.scrollY + topOffset - 100,
-        behavior: 'smooth',
-      });
+  setTimeout(() => {
+    if (hash) {
+      const id = hash.substring(1); // Remove the '#' character
+      const element = document.getElementById(id);
+      if (element) {
+        const topOffset = element.getBoundingClientRect().top;
+        window.scrollTo({
+          top: window.scrollY + topOffset - 100,
+          behavior: 'smooth',
+        });
+      }
     }
-  }
+  }, 100);
 };
 
 export default getUrl;
