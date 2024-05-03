4cd07008cb99b4a4f29a0f94ffb7fd4170a78b7b
Lysenko <lysenkoden@gmail.com>
2024-05-03 23:51:29 +0200

    add a story anf refactor the function getUrl()

--- client/src/data/getUrl.js
+++ client/src/data/getUrl.js
@@ -1,23 +1,3 @@
-// const getUrl = () => {
-//   setTimeout(() => {
-//     const hash = window.location.hash;
-//     if (hash) {
-//       const id = hash.substring(1); // Remove the '#' character
-//       const element = document.getElementById(id);
-//       if (element) {
-//         const topOffset = element.getBoundingClientRect().top;
-//         const yOffset = window.scrollY; // Current scroll position
-//         window.scrollTo({
-//           top: yOffset + topOffset - 100, // Adjust the scroll position as needed
-//           behavior: 'smooth',
-//         });
-//       }
-//     }
-//   }, 100);
-// };
-
-// export default getUrl;
-
 const adjustScrollPosition = () => {
   const hash = window.location.hash;
   if (hash) {
@@ -34,31 +14,22 @@ const adjustScrollPosition = () => {
 };
 
 const getUrl = () => {
-  console.log('getUrl function is called');
+  // Check if all images are loaded
+  const checkImagesLoaded = () =>
+    [...document.images].every((img) => img.complete);
 
-  // Function to check if all images are loaded
-  const checkImagesLoaded = () => {
-    const images = document.querySelectorAll('img');
-    for (const image of images) {
-      if (!image.complete) {
-        return false; // At least one image is still loading
-      }
-    }
-    return true; // All images are loaded
-  };
-
-  // Check if all images are loaded every 100ms
+  // Adjust scroll position once all images are loaded
   const intervalId = setInterval(() => {
     if (checkImagesLoaded()) {
-      clearInterval(intervalId); // Stop checking
-      adjustScrollPosition(); // Adjust scroll position
+      clearInterval(intervalId);
+      adjustScrollPosition();
     }
   }, 100);
 
   // Adjust scroll position immediately if all images are already loaded
   if (checkImagesLoaded()) {
-    clearInterval(intervalId); // Stop checking
-    adjustScrollPosition(); // Adjust scroll position
+    clearInterval(intervalId);
+    adjustScrollPosition();
   }
 };
 
