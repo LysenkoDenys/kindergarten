9a31e5527f70cb81073d224cb58714462911fc7e
Lysenko <lysenkoden@gmail.com>
2024-05-06 07:36:06 +0200

    fix useUser from returning null

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,3 +1,6 @@
 export function useUser() {
-  return { id: document.cookie.match(/userId=(?<id>[^;]+);?$/).groups.id };
+  // return { id: document.cookie.match(/userId=(?<id>[^;]+);?$/).groups.id };
+  const match = document.cookie.match(/userId=(?<id>[^;]+);?$/);
+  const id = match ? match.groups.id : null;
+  return { id };
 }
