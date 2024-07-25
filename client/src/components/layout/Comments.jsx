9e4114eff2fd0d60b154277ba37ff9455b346efa
Lysenko <lysenkoden@gmail.com>
2024-07-25 08:41:09 +0200

    fixed UI in comments block

--- client/src/components/layout/Comments.jsx
+++ client/src/components/layout/Comments.jsx
@@ -14,8 +14,8 @@ const Comments = () => {
   const [text, setText] = useState('');
   const darkTheme = useTheme();
   const themeComment = darkTheme
-    ? 'px-3 py-1.5 rounded-[10px] w-[70%] mr-1 my-3 shadow-[2px_2px_2px_2px_rgba(255,255,255,0.8)] text-black outline-none leading-[18px]'
-    : 'px-3 py-1.5 rounded-[10px] w-[70%] mr-1 my-3 shadow-[5px_5px_5px_5px_rgba(0,0,0,0.4)] outline-none leading-[18px]';
+    ? 'px-3 py-1.5 rounded-[10px] w-[50%] mr-2 my-3 shadow-[2px_2px_2px_2px_rgba(255,255,255,0.8)] text-black outline-none leading-[18px]'
+    : 'px-3 py-1.5 rounded-[10px] w-[50%] mr-2 my-3 shadow-[5px_5px_5px_5px_rgba(0,0,0,0.4)] outline-none leading-[18px]';
 
   const handleChange = (event) => setText(event.target.value);
 
--- client/src/components/layout/SearchBar.jsx
+++ client/src/components/layout/SearchBar.jsx
@@ -37,8 +37,8 @@ const SearchBar = (props) => {
   const darkTheme = useTheme();
 
   const themeInput = darkTheme
-    ? 'flex items-center p-3  bg-white rounded-[10px] w-[50%] mr-1 my-3 shadow-[2px_2px_2px_2px_rgba(255,255,255,0.8)] text-black'
-    : 'flex items-center p-3 bg-white rounded-[10px] w-[50%] mr-1 my-3 shadow-[5px_5px_5px_5px_rgba(0,0,0,0.4)]';
+    ? 'flex items-center p-3  bg-white rounded-[10px] w-[50%] mr-2 my-3 shadow-[2px_2px_2px_2px_rgba(255,255,255,0.8)] text-black'
+    : 'flex items-center p-3 bg-white rounded-[10px] w-[50%] mr-2 my-3 shadow-[5px_5px_5px_5px_rgba(0,0,0,0.4)]';
 
   return (
     <div className="flex justify-center items-center" id="search">
@@ -59,7 +59,7 @@ const SearchBar = (props) => {
           />
         )}
       </div>
-      <Button className="" label="Знайти" actionOnClick={handleFindClick} />
+      <Button label="Знайти" actionOnClick={handleFindClick} />
     </div>
   );
 };
