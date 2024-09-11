0ae6c88eb4b919a4bcdac22da7ebe315cd34a1ff
Lysenko <lysenkoden@gmail.com>
2024-09-11 22:56:15 +0200

    temporary solution with console.log() in getUrl.js to scroll to the right element

--- client/src/data/getUrl.js
+++ client/src/data/getUrl.js
@@ -1,10 +1,13 @@
 const adjustScrollPosition = () => {
   const hash = window.location.hash;
+  // console.log('current hash:', hash);
   if (hash) {
     const id = hash.substring(1);
     const element = document.getElementById(id);
+    console.log('element:', element);
     if (element) {
       const topOffset = element.getBoundingClientRect().top;
+      // console.log('scroll to:', topOffset);
       window.scrollTo({
         top: window.scrollY + topOffset - 100,
         behavior: 'smooth',
