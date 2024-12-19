d479b5ffc54a68acc860bc2313e955d184ff7c52
Lysenko <lysenkoden@gmail.com>
2024-12-19 22:45:52 +0100

    improved Author component: aria, calculations moved from JSX

--- client/src/components/layout/Author.jsx
+++ client/src/components/layout/Author.jsx
@@ -1,6 +1,8 @@
 import { FaHeart } from 'react-icons/fa6';
 
 const Author = () => {
+  const currentYear = new Date().getFullYear() || 2024;
+  const yearText = currentYear === 2024 ? '' : ` - ${currentYear}`;
   return (
     <div className="text-[0.8em] my-1 hover:scale-110 ease-in-out duration-300">
       <a
@@ -8,11 +10,13 @@ const Author = () => {
         href="mailto:lysenkoden@gmail.com"
         title="mail to the author"
       >
-        Made with <FaHeart className="mx-1 text-red-500" /> by Lysenko Denys |
-        2024
-        {new Date().getFullYear() === 2024
-          ? ''
-          : '-' + new Date().getFullYear()}
+        Made with{' '}
+        <FaHeart
+          className="mx-1 text-red-500"
+          style={{ width: '1em', height: '1em' }}
+          aria-label="heart icon"
+        />{' '}
+        by Lysenko Denys | 2024{yearText}
       </a>
     </div>
   );
