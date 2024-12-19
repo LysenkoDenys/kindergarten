411721ef5d501a7350464b842d8c683e6fd4e5e9
Lysenko <lysenkoden@gmail.com>
2024-12-19 23:02:24 +0100

    fixed mr-1 to mr-2 in icon style and added aria-labels

--- client/src/components/layout/MainFooter.jsx
+++ client/src/components/layout/MainFooter.jsx
@@ -9,28 +9,39 @@ import Author from './Author';
 
 const MainFooter = () => {
   const iconStyle =
-    'hover:scale-110 ease-in-out duration-300 mr-1 w-[50px] h-[50px]';
+    'w-[50px] h-[50px] hover:scale-110 ease-in-out duration-300 mr-2';
 
   return (
     <Block>
       <div className="flex flex-wrap items-center justify-around " id="footer">
         <div className="flex text-[50px] cursor-pointer">
-          <a href="mailto:lysenkoden@gmail.com">
-            <TiSocialAtCircular className={iconStyle} title="e-mail" />
+          <a href="mailto:lysenkoden@gmail.com" aria-label="Email Link">
+            <TiSocialAtCircular
+              className={iconStyle}
+              title="Email Lysenko Denys"
+            />
           </a>
           <a
             href="https://www.facebook.com/profile.php?id=100001281440746"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
+            aria-label="Facebook Link"
           >
-            <TiSocialFacebookCircular className={iconStyle} title="facebook" />
+            <TiSocialFacebookCircular
+              className={iconStyle}
+              title="Facebook Profile"
+            />
           </a>
           <a
             href="https://www.linkedin.com/in/lysenko-denys/"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn Link"
           >
-            <TiSocialLinkedinCircular className={iconStyle} title="linkedin" />
+            <TiSocialLinkedinCircular
+              className={iconStyle}
+              title="LinkedIn Profile"
+            />
           </a>
         </div>
         <div className="flex">
